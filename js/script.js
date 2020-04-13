b936ce36f014e6724bad08ecc98db633f64509c2
Francois Disubi <disubif@gmail.com>
2020-04-13 10:52:39 +0200

    Prémière version de la fonctionalité modifier et supprimer

--- js/script.js
+++ js/script.js
@@ -2,18 +2,20 @@ const btnAjouter = document.querySelector("#Ajouter");
 const formAjouter = document.querySelector("#formulaire");
 const btnanuler = document.querySelector("#anuler");
 const form = document.querySelector("form");
- 
+
+const modale1 = document.querySelector("#modale");
 btnAjouter.addEventListener("click", function(e) 
 {
     btnAjouter.style.display = "none";
     formAjouter.style.display = "block";
+    form.reset();
 });
 btnanuler.addEventListener("click", function(e) 
 {
     btnAjouter.style.display = "block";
     formAjouter.style.display = "none";
+    form.reset();
 });
-
 
 
 form.addEventListener("submit", function(e)
@@ -50,8 +52,6 @@ form.addEventListener("submit", function(e)
                }
            }
        }
-
-       
    } 
    //console.log(tr);
    
@@ -60,8 +60,47 @@ form.addEventListener("submit", function(e)
        btnAjouter.style.display = "block";
        formAjouter.style.display = "none";
        this.reset();
+       const employers = document.querySelectorAll("tbody th");
+       let suprimer = document.querySelector("#suprimer");
+       for(employer of employers)
+       {
+            employer.addEventListener("click", function (e) 
+            {
+                modale1.style.display = "flex";
+                let obj = this;
+                suprimer.addEventListener("click", function(e)
+                {
+                    // obj.parentNode.remove();
+                    // tr.removeChild(obj);
+                    // document.querySelector("tbody").removeChild(obj.parentNode);
+                    document.querySelector("tbody").remove(obj.parentNode);
+                    modale1.style.display = "none";
+                });
+                let modifier = document.querySelector("#modifier");
+                modifier.addEventListener("click", function(e)
+                {
+                  let a = obj.parentNode.childNodes;
+                  for (let j = 0; j < a.length; j++)
+                  {
+                    form.elements[j].value = a[j].textContent;
+                  }
+                //   console.log(z.textContent)
+                // console.log(a[0].textContent);
+                // modale1.style.display = "none";
+                formAjouter.style.display = "block";
+                    modale1.style.display = "none";
+                    btnanuler.textContent = "Supprimer"
+                    document.querySelector("tbody").remove(obj.parentNode);
+                })
+                btnanuler.textContent = "Anuler"
+            })
+        }
    }
   e.preventDefault();
-   
-    
+})
+let quiter = document.querySelector("#quiter");
+
+quiter.addEventListener("click", function(e)
+{
+    modale1.style.display = "none";
 })
